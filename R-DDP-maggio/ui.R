502fd3c7e37bd4bf8e2ef5f015c47a42fb81646a
Matteo <MGrazzini@users.noreply.github.com>
2015-05-13 22:48:52 +0200

    Update ui.R

--- R-DDP-maggio/ui.R
+++ R-DDP-maggio/ui.R
@@ -1,20 +1,20 @@
 ##ui.R
 shinyUI(
   pageWithSidebar(
-    headerPanel("The Central Limit Theorem"),
+    headerPanel("The Law of Large Numbers"),
     sidebarPanel(
       numericInput('lambda', 'Lambda:', value = 1, min = 0.1, max = 10, step = 0.1)
     ),
     mainPanel(
-      withMathJax("Verification of the Central Limit Theorem using an 
+      withMathJax("Verification of the Law of Large Numbers using an 
                   exponential distribution $$f(x) = \\lambda e^{-\\lambda x}.$$"),
       withMathJax("We know from theory that the expected value for
                   the exponential distribution is $$1/\\lambda.$$"),
       withMathJax("After submitting a value for $$\\lambda (\\lambda > 0)$$ on the left,
                   the expected value for the associated exponential distribution is
                   calculated, along with 1000 simulations of 50 exponentials. The two
-                  values are shown below for comparison and verification of the Central
-                  Limit Theorem."),
+                  values are shown below for comparison and verification of the Law of
+                  Large Numbers."),
       p('\n'),
       p('\nExpected value from function: '),
       textOutput('textfun'),
